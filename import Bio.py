395b1fe87014083b2c060a3377897c298ff879ff
irenerivaas <110342189+irenerivaas@users.noreply.github.com>
2023-04-03 19:32:34 +0200

    Add files via upload

--- import Bio.py
+++ import Bio.py
@@ -1,12 +1,13 @@
 import Bio
-from Bio import Entrez
 from Bio import SeqIO
 import fastaparser
+from Bio.Align.Applications import MafftCommandline
+from Bio import AlignIO
 
 for record in SeqIO.parse("sequence.fasta", "fasta"):
     print(record.id)
 
-### Muestra de Wuhan
+### Muestra de referencia
 with open("sequence.fasta") as fasta_file:
         parser = fastaparser.Reader(fasta_file)
         for seq in parser:
@@ -14,4 +15,14 @@ with open("sequence.fasta") as fasta_file:
             print('ID:', seq.id)
             print('Descripción:', seq.description)
             print('Secuencia:', seq.sequence_as_string())
+            #Filtrar las secuencias para asegurarnos de que realmente son completas
             print()
+
+#Alinearlas utilizando MAFFT
+mafft_cline = MafftCommandline(input="sequence.fasta")
+stdout, stderr = mafft_cline()
+with open("aligned.fasta","w") as handle:
+     handle.write(stdout)
+#Almacenar el resultado
+alignment=AlignIO.read("sequence.fasta","fasta")
+print(alignment)
